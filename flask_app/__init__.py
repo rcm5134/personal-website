c3bdeae00093ed5cc8fad56be1c70dd22dac76a8
rcm7133 <moorma14@msu.edu>
2025-03-01 22:28:24 -0500

    Added Sudoku project

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -29,10 +29,11 @@ def create_app(debug=False):
 	db = database()
 	db.createTables(purge=True)
 
-	db.createProject("Pacman AI Maze Search", "pacman.png", "Found optimal cost paths for a Pacman Agent by implementing Dijkstra's algorithm, A*, DFS, BFS, and more!", "https://www.github.com", 1,['Python'])
+	db.createProject("Pacman AI Maze Search", "pacman.png", "Found optimal cost paths for a Pacman Agent by implementing Dijkstra's algorithm, A*, DFS, BFS, and more!", "https://github.com/rcm5134/PacmanAI", 1,['Python'])
 	db.createProject("Connect Four Machine Learning", "connectfour.png", 'Used Machine Learning techniques such as CNN, MLP, SVM, Decision Tree, and Reinforcement Learning to create a Connect Four Bot.', "https://github.com/kjowak/cse-404-fs24-connect-four-bot", 1,['Python'])
 	db.createProject("Traffic Jam Assist Prototype", "tja.png", 'Collaborated with four team members and customers to create a Software Requirement Specification document and rapid prototype for safety critical car systems.', "https://cse.msu.edu/~tamayoa3/prototype.html", 1, ["C#", "Unity", "UML"])
 	db.createProject("Personal Website", "personal.png", 'Mobile friendly personal website for Riley Moorman using HTML, CSS, Python, SQL, and vanilla JavaScript', "https://github.com/rcm5134/personal-website", 0,["JavaScript", "SQL", "CSS", "HTML", "Python"])
+	db.createProject("C++ Sudoku Application", "sudoku.png", 'Collaborated with four developers to create a Sudoku game using C++ and object oriented programming.', "https://gitlab.msu.edu/tranbrya/coeus", 1,["C++"])
 
 	socketio.init_app(app)
 
